495d1846eaa4caf36e1b5d11df77d285b3c0cf67
Juan Alvarenga <juanenrique94@gmail.com>
2023-01-31 18:41:43 -0600

    uso de ciclos

---
+++ intro-python/ciclos.py
@@ -0,0 +1,34 @@
+print('ciclos for y while')
+
+lista_nombres = ["Juan", "Pedro", "Enrique"]
+numeros = [1, 2, 3, 4, 5, 5, 6, 4, 3, 5, 63, 2, 4]
+
+diccionario = {
+    "nombre":"Juan",
+    "apellido":"Alvarenga",
+    "edad": 28
+}
+
+# for value in diccionario:
+#     print( diccionario[value])
+
+for value in diccionario.values():
+    print(value)
+
+for key, value in diccionario.items():
+    print("key: "+key+ ", value: "+ value)
+    print(f"Key: {key}, value: {value}")
+
+# ciclo FOR mediante uso de "range"
+# for index in range(0, 10):
+#     print(index)
+
+# for value in lista_nombres:
+#     print(value)
+
+# contador = 0
+#
+# while contador < len(numeros):
+#     print(numeros[contador])
+#     contador = contador + 1
+
